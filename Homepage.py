3d7d4a0896c529a71567d95e6f49cfac75243cf1
Manish Kumar <91662505+manishkumarya@users.noreply.github.com>
2024-05-16 11:40:23 +0530

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import base64
+import plotly.express as px
+
+st.set_page_config(
+    page_title="HomePage",
+    
+)
+st.title("A Question Answering System for User-Uploaded PDF and URL using LLMs")
+
+
+df = px.data.iris()
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("image.jpg")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img}");
+background-size: 120%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
